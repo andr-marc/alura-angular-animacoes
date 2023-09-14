36eb0a2643c244157b5e6c9bdba7de3eefedba97
André Marc <andreluis0015@gmail.com>
2023-09-14 20:31:19 -0300

    + animacao com offset e cubic-bezier

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,5 +1,6 @@
 import {
   animate,
+  keyframes,
   state,
   style,
   transition,
@@ -46,3 +47,31 @@ export const checkButtonTrigger = trigger('checkButton', [
     }))
   ])
 ])
+
+export const filterTrigger = trigger('filterAnimation', [
+  transition(':enter', [
+    style({ opacity: 0, width: 0 }),
+    animate(
+      '400ms ease-out',
+      keyframes([
+        style({ offset: 0, opacity: 0, width: 0 }),
+        style({
+          offset: 0.8,
+          opacity: 0.5,
+          width: '*',
+        }),
+        style({
+          offset: 1,
+          opacity: 1,
+          width: '*',
+        }),
+      ])
+    ),
+  ]),
+  transition(':leave', [
+    animate(
+      '400ms cubic-bezier(.13,.9,.8,.1)',
+      style({ opacity: 0, width: 0 })
+    ),
+  ]),
+]);
